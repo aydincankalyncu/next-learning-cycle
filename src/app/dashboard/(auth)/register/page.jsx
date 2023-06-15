61c985bb133b4a9b488ffe48de02b86e3c0c2fd5
Aydıncan Kalyoncu <aydincan.kalyoncu@arvento.onmicrosoft.com>
2023-06-15 17:37:37 +0300

    Authentication operations in progress

--- src/app/dashboard/(auth)/register/page.jsx
+++ src/app/dashboard/(auth)/register/page.jsx
@@ -1,9 +1,64 @@
-import React from 'react'
+"use client";
+import React, { useState } from "react";
+import styles from "./page.module.css";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 const Register = () => {
-  return (
-    <div>Register</div>
-  )
-}
+  const [error, setError] = useState(false);
 
-export default Register
+  const router = useRouter()
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const name = e.target[0].value;
+    const email = e.target[1].value;
+    const password = e.target[2].value;
+
+    try {
+      const res = await fetch("/api/auth/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({name, email, password}),
+      });
+
+      res.status === 201 && router.push("/dashboard/login?success=Account has been created");
+
+    } catch (err) {
+      setError(true);
+    }
+  };
+
+  return (
+    <div className={styles.container}>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <input
+          type="text"
+          required
+          placeholder="username"
+          className={styles.input}
+        />
+        <input
+          type="email"
+          required
+          placeholder="email"
+          className={styles.input}
+        />
+        <input
+          type="password"
+          required
+          placeholder="password"
+          className={styles.input}
+        />
+        <button className={styles.button}>Register</button>
+        <Link className={styles.container} href="/dashboard/login">
+          Login with an existing account
+        </Link>
+      </form>
+      {error && <p>Error occured.</p>}
+    </div>
+  );
+};
+
+export default Register;
